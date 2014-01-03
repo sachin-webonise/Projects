af63c9f9e11d275b4bd3d43ecca143b3a29f031d
sachin.webonise <sachin.wagh@weboniselab.com>
2014-01-03 21:32:02 +0530

    commiting project stuff

---
+++ rails_stuff/blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'ed7eb863f7332139fd2819e5fd16c74c1f4e460c0de1887225901951f3c963fc758211b13cccf246aa4623132f864a615e8f2dd7816d2ef2a93a84373d2abf2b'
---
+++ rails_stuff/my_blog/app/controllers/posts_controller.rb
@@ -0,0 +1,9 @@
+class PostsController < ApplicationController
+	def new
+	end
+	def create
+  	@post = Post.new(params[:post])
+  	@post.save
+  	redirect_to @post
+	end
+end
